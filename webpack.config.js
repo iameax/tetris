31b7933f0d310b653d50d914349c6dc0d8da09c9
iameax <goo.eax@gmail.com>
2019-10-12 12:30:34 +0300

    eslint added, source-map configured

--- webpack.config.js
+++ webpack.config.js
@@ -14,9 +14,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.ts$/,
+        test: /\.ts(x?)$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
+        include: /src/,
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
       },
       {
         test: /\.css$/,
